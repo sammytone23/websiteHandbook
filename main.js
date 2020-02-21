922e727894cc4b45da625bd7a2c3ed88d952a108
sammytone23 <sammytone23@gmail.com>
2020-02-22 09:43:14 +1100

    Try to use Jquery lol

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-//let data = "";
+let source = "PageData.json"
+let pageData = "";
 $.getJSON("PageData.json",function(json){
     console.log(json.pages.pages.home.id);
 });
-
+(function() {
+  $.getJSON(source)
+    .done(function( data ) {
+        pageData=data
+        console.log(pageData)
+    });
+})();
 // let current = "";
 // current="home";
 // data.pages.pages.forEach(page =>{
 //     alert(page);
 // });
+      // $.each( data.items, function( i, item ) {
+      //   $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+      //   if ( i === 3 ) {
+      //     return false;
+      //   }
+      // });
