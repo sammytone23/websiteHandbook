3609e31bb874a8858c8ff6f1ba53ac0d5947d366
sammytone23 <sammytone23@gmail.com>
2020-02-25 09:03:03 +1100

    Update main.js to use window.fetch

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
 let source = "PageData.json"
-let pageData = "";
-$.getJSON("PageData.json",function(json){
-    console.log(json.pages.pages.home.id);
+window.fetch('PageData.json').then((x)=>x.json()).then((pageData)=>{
+    main(pageData)
 });
-(function() {
-  $.getJSON(source)
-    .done(function( data ) {
-        pageData=data
-        console.log(pageData)
-    });
-})();
-// let current = "";
-// current="home";
-// data.pages.pages.forEach(page =>{
-//     alert(page);
-// });
-      // $.each( data.items, function( i, item ) {
-      //   $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-      //   if ( i === 3 ) {
-      //     return false;
-      //   }
-      // });
+
+main = function(data){
+let current = "";
+current="home";
+data.pages.pages.forEach(page =>{
+    alert(page);
+});}
